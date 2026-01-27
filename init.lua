cec9ff8a9a582ba85400300b8a88b9e774857b07
WJH <3506456886@qq.com>
2026-01-27 18:16:32 +0800

    feat: 适应键盘媒体键

--- init.lua
+++ init.lua
@@ -6,18 +6,54 @@ function controlEVPlayer(action)
     hs.osascript.applescriptFromFile(scriptPath)
 end
 
--- Bind media keys to EVPlayer2 controls
--- F7: Previous track
+-- Media key event tap using systemDefined events
+local mediaKeyTap = hs.eventtap.new({hs.eventtap.event.types.systemDefined}, function(event)
+    local data = event:systemKey()
+
+    -- Debug: print what we receive (can be removed later)
+    if data and data.down then
+        print("Media key detected: " .. tostring(data.key))
+    end
+
+    if not data or not data.down then
+        return false
+    end
+
+    -- Check if EVPlayer2 is running
+    local evplayer = hs.application.get("EVPlayer2")
+    if not evplayer then
+        return false  -- Let other apps handle it
+    end
+
+    local key = data.key
+
+    -- Handle different media key names
+    if key == "PLAY" or key == "PAUSE" or key == "PLAY_PAUSE" then
+        controlEVPlayer("PlayPause")
+        return true  -- Block the event from other apps
+    elseif key == "NEXT" or key == "FAST" then
+        controlEVPlayer("Next")
+        return true
+    elseif key == "PREVIOUS" or key == "REWIND" then
+        controlEVPlayer("Previous")
+        return true
+    end
+
+    return false
+end)
+
+-- Start the media key tap
+mediaKeyTap:start()
+
+-- Also keep F-key bindings as backup
 hs.hotkey.bind({}, "f7", function()
     controlEVPlayer("Previous")
 end)
 
--- F8: Play/Pause
 hs.hotkey.bind({}, "f8", function()
     controlEVPlayer("PlayPause")
 end)
 
--- F9: Next track
 hs.hotkey.bind({}, "f9", function()
     controlEVPlayer("Next")
 end)
